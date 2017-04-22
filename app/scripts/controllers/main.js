6f15b8cd78e88bf5c7f722a547d7e082751f2a4a
Antonio Fiñana <antonio.finana@atos.net>
2017-04-22 17:47:24 +0200

    Visual Studio project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mylocationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mylocationApp
+ */
+angular.module('mylocationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
